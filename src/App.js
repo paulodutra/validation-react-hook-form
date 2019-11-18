cd13dfd4221b9ae7593bb2a0c9609622541234dc
Paulo Dutra <paulodutra.info@gmail.com>
2019-11-18 09:13:05 -0300

    exemplo de validação com watch e errors, concluído

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import useForm from 'react-hook-form';
+
 
 function App() {
+
+  //Destructor no hook importado
+  const { register, handleSubmit, watch, errors } = useForm();
+
+  //Função que irá receber os dados após a validação do formulário
+  const onSubmitForm = (data) => {
+      console.log('dados do form' , data);
+
+  }
+
+  console.log(watch('name'))
+
+  /**
+   * ref: atributo jsx que registra o que será validado no input atráves da função register
+   * e de um objeto com os tipos de validações a serem aplicados
+   * 
+   * errors: objeto criado, com base nos names dos inputs, caso não esteja vazio
+   * significa que houve erro de validação. OBS: Esse processo acontece logo após a submissão
+   * do formulário
+   * 
+   * watch: Atráves do name atribuito ao input é possível observar(assistir) as alterações 
+   * que são feitas no campo. Funciona como um observable
+   */
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="form">
+        <form onSubmit={handleSubmit(onSubmitForm)}>
+            <input type="text" name="name" ref={register({ required: true })} placeholder="Name" />
+            {errors.name && <span>The field name is required</span>}
+            <input type="number" name="age" ref={register({ required: true, min: 2})} placeholder="Age" />
+            {errors.age && <span> The field age is required and min age 2</span>}
+            <button type="submit">Send</button>
+        </form>
     </div>
   );
 }
